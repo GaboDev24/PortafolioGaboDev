302b8fa142eaeaa1c34aab11699ac69bffba6035
Gabriel Reina <135169770+GaboDev24@users.noreply.github.com>
2025-08-28 16:05:53 -0300

    añadido busqueda de header

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,6 +1,23 @@
 const toggleBtn = document.querySelector(".menu-toggle");
 const nav = document.querySelector("header nav");
+const overlay = document.querySelector(".overlay");
 
+// abrir / cerrar menú
 toggleBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+// cerrar al hacer clic en overlay
+overlay.addEventListener("click", () => {
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+// cerrar al hacer clic en un enlace del menú
+document.querySelectorAll("header nav a").forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+  });
 });
